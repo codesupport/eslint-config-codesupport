1fb92e60b80a54edbda90a908ce3101a51037d67
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-10-11 20:47:19 +0100

    Created the logic for the ESLint config.

---
+++ rules/best-practices.js
@@ -0,0 +1,20 @@
+module.exports = {
+	rules: {
+		"block-scoped-var": "error",
+		"curly": "error",
+		"dot-notation": "error",
+		"no-empty-function": "error",
+		"no-eval": "error",
+		"no-extend-native": "error",
+		"no-invalid-this": "error",
+		"no-labels": "error",
+		"no-multi-spaces": "error",
+		"no-new-func": "error",
+		"no-new-wrappers": "error",
+		"no-param-reassign": "error",
+		"no-redeclare": "error",
+		"no-self-assign": "error",
+		"no-self-compare": "error",
+		"no-useless-concat": "error"
+	}
+};
