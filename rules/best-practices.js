653151b5c568766c1ad64492b9dcde195c20aa71
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-12-20 13:55:39 +0000

    Set eqeqeq to error always.

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -3,6 +3,7 @@ module.exports = {
 		"block-scoped-var": "error",
 		"curly": "error",
 		"dot-notation": "error",
+		"eqeqeq": ["error", "always"],
 		"no-empty-function": "error",
 		"no-eval": "error",
 		"no-extend-native": "error",
